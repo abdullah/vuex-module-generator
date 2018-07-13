9cb834f04eb30c2b6a30464f5a82d590a31db8a4
Abdullah Mara <a.mara@vispera.co>
2018-07-13 18:12:47 +0300

    add .babelrc

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "index.js",
+    filename: "vue-module-factory.min.js",
     library: "VuexModuleFactory",
     libraryTarget: "umd",
   },
